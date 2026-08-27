6c92a4c3e5173c8640379a369993e807bd91299a
Stefpap <stefpap6sp@gmail.com>
2026-08-27 15:02:28 +0300

    Add Entities and GameStoreContext

---
+++ WebApplication1/Data/GameStoreContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Data;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) 
+    : DbContext(options)
+{
+
+    // Οι πίνακες της βάσης δεδομένων μας
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+}
--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f548d77cec24943aec63b916ff2fed34abde63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944472b49ca3fa36b380348ebd48bd2a6dd119b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
