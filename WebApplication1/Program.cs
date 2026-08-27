ed069348d7e02272924ba96123f75911d54b9320
Stefpap <stefpap6sp@gmail.com>
2026-08-27 15:30:52 +0300

    Add SQLite database, EF Core configuration and initial migration

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,7 +1,10 @@
 using WebApplication1.Endpoints;
-
+using WebApplication1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944472b49ca3fa36b380348ebd48bd2a6dd119b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c92a4c3e5173c8640379a369993e807bd91299a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
