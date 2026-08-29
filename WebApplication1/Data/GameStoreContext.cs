71111fc085a2eb1804447b317bc21a354eac3740
Stefpap <stefpap6sp@gmail.com>
2026-08-29 19:04:33 +0300

    Add seed data for genres and generate migration

--- WebApplication1/Data/GameStoreContext.cs
+++ WebApplication1/Data/GameStoreContext.cs
@@ -10,4 +10,15 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options)
     // Οι πίνακες της βάσης δεδομένων μας
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new  { Id = 1, Name = "Action" },
+            new  { Id = 2, Name = "Adventure" },
+            new  { Id = 3, Name = "RPG" },
+            new  { Id = 4, Name = "Strategy" },
+            new  { Id = 5, Name = "Simulation" }
+        );
+    }
 }
