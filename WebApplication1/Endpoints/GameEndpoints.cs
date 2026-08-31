42635fd0cdc8ba8060de7b6a4ac852397377d03c
Stefpap <stefpap6sp@gmail.com>
2026-08-31 16:23:06 +0300

    map Game entity to GameDto for clean API response

--- WebApplication1/Endpoints/GameEndpoints.cs
+++ WebApplication1/Endpoints/GameEndpoints.cs
@@ -57,7 +57,17 @@ public static class GameEndpoints
                     };
                     dbContext.Games.Add(game);
                     dbContext.SaveChanges();
-                    return Results.CreatedAtRoute("GetName", new { id = game.Id }, game);
+
+                    GameDto gameDto = new(
+                        game.Id,
+                        game.Name,
+                        game.Genre!.Name,
+                        game.Price,
+                        game.ReleaseDate
+                    );
+
+
+                    return Results.CreatedAtRoute("GetName", new { id = game.Id }, gameDto);
             });
             
             // PUT /games/
